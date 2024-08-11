6cc999c7e02775c94fec1a3c4436af5a73be46a8
Lele <wbnominat@bk.ru>
2024-08-11 18:00:38 +0300

    Add files via upload

---
+++ module 1 4.py
@@ -0,0 +1,9 @@
+# my_string = input ("Какая сегодня погода?")
+# print(len(my_string))
+#print ("какя сегодня погода" .lower())
+# print ("какя сегодня погода" .upper())
+print("Cегодня очень пасмурная погода" .replace(" " , ""))
+my_string = "Sun"
+print(my_string[0])
+print(my_string[-1])
+
